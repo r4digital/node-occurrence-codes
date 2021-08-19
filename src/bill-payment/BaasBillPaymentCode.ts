d625ccba4318b20287a43a5df594417b5acc13d1
Fabio Brandt Giordani <00037959@randon.com.br>
2021-08-19 12:10:21 -0300

    Create Bill Payment codes

---
+++ src/bill-payment/BaasBillPaymentCode.ts
@@ -0,0 +1,35 @@
+export enum BaasBillPaymentCode {
+  /**
+   * Criado
+   */
+  BBP0001 = 'BBP0001',
+  /**
+   * Autorizado
+   */
+  BBP0002 = 'BBP0002',
+  /**
+   * Confirmado
+   */
+  BBP0003 = 'BBP0003',
+  /**
+   * Não confirmado
+   */
+  BBP0004 = 'BBP0004',
+  /**
+   * Transferido
+   */
+  BBP0005 = 'BBP0005',
+  /**
+   * Rejeitado
+   */
+  BBP0006 = 'BBP0006',
+  /**
+   * Realizado
+   */
+  BBP0007 = 'BBP0007',
+  /**
+   * Erro genérico
+   */
+  BBP9999 = 'BBP9999'
+}
+export default BaasBillPaymentCode;
