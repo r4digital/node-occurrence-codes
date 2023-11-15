6b96268a9aac60706307617302c9e59e6e11edf1
Andre Braghini <t0003231@randon.com.br>
2023-11-15 02:40:01 -0300

    fix: export investment occurrence codes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import transfer from './transfer';
 import billPayment from './bill-payment';
 import pix from './pix';
+import investment from './investment';
 
 const occurrence = Object.freeze({
   transfer,
   billPayment,
-  pix
+  pix,
+  investment,
 });
 
 export default occurrence;
